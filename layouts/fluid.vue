baa8947ad93c845d65a5d0920f4d5b06e1445769
Christopher Ritter <chris@christopherritter.com>
2021-01-31 18:43:19 -0500

    updated home page

--- layouts/fluid.vue
+++ layouts/fluid.vue
@@ -23,7 +23,7 @@
 
           <v-list>
             <v-subheader>PROFILE</v-subheader>
-            <v-list-item-group v-model="selectedItem" color="primary">
+            <v-list-item-group color="primary">
               <v-list-item v-for="(item, i) in profileItems" :key="i">
                 <v-list-item-icon>
                   <v-icon v-text="item.icon"></v-icon>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <v-responsive :aspect-ratio="16 / 6">
     <v-sheet color="dark" elevation="1" width="100%" height="100%">
-      <v-container>
+      <v-container style="height:100%">
         <v-row>
-          <v-col>
-            <h1>Home</h1>
+          <v-col cols="6" sm="6">
+            <h5>Featured Quest</h5>
+            <h1 class="display-3">Welcome to the Learning Tree Farm</h1>
+            <h3>by The Learning Tree</h3>
+            <v-btn color="primary">More Info</v-btn>
+          </v-col>
+          <v-col cols="6" sm="6">
+            <v-img :src="require('~/assets/img/welcome-sign.png')" aspect-ratio="1.4"></v-img>
           </v-col>
         </v-row>
       </v-container>
