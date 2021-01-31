2c743e9850e7785293c4714d315e97b035b77b22
Christopher Ritter <chris@christopherritter.com>
2021-01-31 18:03:52 -0500

    created fluid layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>Home</h1>
+  <v-responsive :aspect-ratio="16 / 6">
+    <v-sheet color="dark" elevation="1" width="100%" height="100%">
+      <v-container>
+        <v-row>
+          <v-col>
+            <h1>Home</h1>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
+  </v-responsive>
 </template>
 
 <script>
 export default {
-  name: 'home',
+  name: "home",
+  layout: "fluid"
 };
 </script>
