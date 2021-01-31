18c196d501fa3e722c5001283728fc97ff62605c
Christopher Ritter <chris@christopherritter.com>
2021-01-31 12:22:48 -0500

    created log in form

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,28 @@ export default {
     ]
   },
 
+  hooks: {
+    generate: {
+      async done() {
+        // This makes sure nuxt generate does finish without running into a timeout issue.
+        // See https://github.com/nuxt-community/firebase-module/issues/93
+        const appModule = await import('./.nuxt/firebase/app.js')
+        const { session } = await appModule.default(
+          builder.options.firebase.config,
+          {
+            res: null,
+          }
+        )
+        try {
+          session.database().goOffline()
+        } catch (e) {}
+        try {
+          session.firestore().terminate()
+        } catch (e) {}
+      },
+    },
+  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
