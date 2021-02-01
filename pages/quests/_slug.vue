525665004def8b8af87afbc189ac43c480e65719
Christopher Ritter <chris@christopherritter.com>
2021-02-01 12:35:15 -0500

    created quests slug

---
+++ pages/quests/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ quests[this.slug].title }}</h1>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const slug = params.slug; // When calling /abc the slug will be "abc"
+    return { slug };
+  },
+  computed: {
+    quests() {
+      return this.$store.state.quests;
+    }
+  }
+};
+</script>
--- store/state.js
+++ store/state.js
@@ -1,3 +1,41 @@
 export default () => ({
-  authUser: null
+  authUser: null,
+  quests: [
+    {
+      id: 0,
+      title: "Welcome to the Learning Tree Farm",
+      author: "The Learning Tree Farm",
+      image: "welcome-sign.png",
+      categories: ["family-friendly", "outdoor", "educational"],
+      favorite: false,
+      featured: true
+    },
+    {
+      id: 1,
+      title: "Celebrate the Spring Equinox",
+      author: "The Learning Tree Farm",
+      image: "baby-goats.png",
+      categories: ["family-friendly", "outdoor", "educational"],
+      favorite: false,
+      featured: true
+    },
+    {
+      id: 2,
+      title: "How Does Your Garden Grow?",
+      author: "The Learning Tree Farm",
+      image: "garden.png",
+      categories: ["family-friendly", "outdoor", "educational"],
+      favorite: false,
+      featured: true
+    },
+    {
+      id: 3,
+      title: "The Great Sheep Escape",
+      author: "The Learning Tree Farm",
+      image: "sheep.png",
+      categories: ["family-friendly", "outdoor", "adventure"],
+      favorite: false,
+      featured: true
+    }
+  ]
 })
