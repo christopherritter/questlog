2f54955b96539f1a5d27ef14acc562a96c6de9fb
Christopher Ritter <chris@christopherritter.com>
2021-02-03 14:41:49 -0500

    created QuestHeader component

---
+++ components/QuestHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="6" sm="6" class="my-auto">
+        <div class="d-flex flex-row mb-2">
+          <v-icon medium color="green darken-2">mdi-crown</v-icon>
+          <h5 class="featured-quest green--text text--darken-2 mt-1 ml-1">
+            Featured Quest
+          </h5>
+        </div>
+        <h1 class="display-3 mb-3">{{ quests[questId].title }}</h1>
+        <h3 class="subtitle-1 mb-sm-8 mb-lg-12">
+          by {{ quests[questId].author }}
+        </h3>
+        <v-btn color="primary">Play Quest</v-btn>
+        <v-btn outlined>Read Story</v-btn>
+      </v-col>
+      <v-col cols="6" sm="6" class="pb-0">
+        <v-img
+          class="mt-12"
+          :src="require('~/assets/img/' + quests[questId].image)"
+          aspect-ratio="1.4"
+        ></v-img>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "QuestHeader",
+  props: ["questId"],
+  computed: {
+    quests() {
+      return this.$store.state.quests;
+    }
+  }
+};
+</script>
+
+<style></style>
--- pages/quests/_slug.vue
+++ pages/quests/_slug.vue
@@ -1,43 +1,18 @@
 <template>
-  <v-container fluid class="fill-height">
-    <v-row style="height:auto">
-      <v-sheet color="dark" elevation="1" height="100%" width="100%">
-        <v-container>
-          <v-row>
-            <v-col cols="6" sm="6" class="my-auto">
-              <div class="d-flex flex-row mb-2">
-                <v-icon medium color="green darken-2">mdi-crown</v-icon>
-                <h5
-                  class="featured-quest green--text text--darken-2 mt-1 ml-1"
-                >
-                  Featured Quest
-                </h5>
-              </div>
-              <h1 class="display-3 mb-3">{{ quests[this.slug].title }}</h1>
-              <h3 class="subtitle-1 mb-sm-8 mb-lg-12">
-                by {{ quests[this.slug].author }}
-              </h3>
-              <v-btn color="primary">Play Quest</v-btn>
-              <v-btn outlined>Read Story</v-btn>
-            </v-col>
-            <v-col cols="6" sm="6" class="pb-0">
-              <v-img
-                class="mt-12"
-                :src="require('~/assets/img/' + quests[this.slug].image)"
-                aspect-ratio="1.4"
-              ></v-img>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-sheet>
-    </v-row>
-    <v-row style="height:52%">
-      <GoogleMap :quest-id="this.slug" />
-    </v-row>
+  <v-container fluid class="fill-height pa-0">
+    <v-layout column class="fill-height">
+      <v-flex class="white--text gray flex shrink darken-3">
+        <QuestHeader :quest-id="this.slug" />
+      </v-flex>
+      <v-flex class="flex">
+        <GoogleMap :quest-id="this.slug" />
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
+import QuestHeader from "@/components/QuestHeader.vue";
 import GoogleMap from "@/components/GoogleMap.vue";
 
 export default {
@@ -45,12 +20,13 @@ export default {
     const slug = params.slug; // When calling /abc the slug will be "abc"
     return { slug };
   },
-  layout: "fluid",
-  components: { GoogleMap },
-  computed: {
-    quests() {
-      return this.$store.state.quests;
+  data() {
+    return {
+      readMode: false,
+      playMode: false,
     }
-  }
+  },
+  layout: "fluid",
+  components: { QuestHeader, GoogleMap },
 };
 </script>
