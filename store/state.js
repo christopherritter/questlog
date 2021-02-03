7362e927349cc3fb902f60f1327ba3472e0438d4
Christopher Ritter <chris@christopherritter.com>
2021-02-03 12:23:02 -0500

    added blank entries for each location

--- store/state.js
+++ store/state.js
@@ -82,31 +82,31 @@ var locations = [
       lat: 39.69705252378522,
       lng: -84.30895915916378,
     },
-    image: "",
+    image: null,
     marker: markers.road,
     entries: [],
   },
   {
     id: 1,
-    name: "Event Parking",
+    name: "Event Parking Lot",
     isLandmark: true,
     position: {
       lat: 39.69669665514492,
       lng: -84.30802298163705,
     },
-    image: "",
+    image: null,
     marker: markers.parking,
     entries: [],
   },
   {
     id: 2,
-    name: "Parking",
+    name: "Parking Lot",
     isLandmark: true,
     position: {
       lat: 39.69683532141846,
       lng: -84.30663546124721,
     },
-    image: "",
+    image: null,
     marker: markers.parking,
     entries: [],
   },
@@ -118,7 +118,7 @@ var locations = [
       lat: 39.69698503159554,
       lng: -84.30554298831034,
     },
-    image: "",
+    image: null,
     marker: markers.info,
     entries: [],
   },
@@ -130,7 +130,7 @@ var locations = [
       lat: 39.697199918787966,
       lng: -84.30579653342916,
     },
-    image: "",
+    image: null,
     marker: markers.house,
     entries: [],
   },
@@ -142,7 +142,7 @@ var locations = [
       lat: 39.69731220085636,
       lng: -84.3058882373532,
     },
-    image: "",
+    image: null,
     marker: markers.restroom,
     entries: [],
   },
@@ -154,7 +154,7 @@ var locations = [
       lat: 39.69733858404809,
       lng: -84.30582683559051,
     },
-    image: "",
+    image: null,
     marker: markers.smog,
     entries: [],
   },
@@ -166,7 +166,7 @@ var locations = [
       lat: 39.697139635581166,
       lng: -84.30512614831034,
     },
-    image: "",
+    image: null,
     marker: markers.barn,
     entries: [],
   },
@@ -178,7 +178,7 @@ var locations = [
       lat: 39.69733956075378,
       lng: -84.30457802856016,
     },
-    image: "",
+    image: null,
     marker: markers.barn,
     entries: [],
   },
@@ -190,7 +190,7 @@ var locations = [
       lat: 39.69743149695268,
       lng: -84.30468044539928,
     },
-    image: "",
+    image: null,
     marker: markers.gate,
     entries: [],
   },
@@ -202,7 +202,7 @@ var locations = [
       lat: 39.69697765198743,
       lng: -84.30461785733092,
     },
-    image: "",
+    image: null,
     marker: markers.corn,
     entries: [],
   },
@@ -214,79 +214,79 @@ var locations = [
       lat: 39.69764999930142,
       lng: -84.30560789744072,
     },
-    image: "",
+    image: null,
     marker: markers.tractor,
     entries: [],
   },
   {
-    id: 11,
+    id: 12,
     name: "Pavilion",
     isLandmark: true,
     position: {
       lat: 39.69758987042334,
       lng: -84.30520121044576,
     },
-    image: "",
+    image: null,
     marker: markers.houseOutline,
     entries: [],
   },
   {
-    id: 12,
+    id: 13,
     name: "Bonfire Pit",
     isLandmark: true,
     position: {
       lat: 39.697573917855074,
       lng: -84.3049683622055,
     },
-    image: "",
+    image: null,
     marker: markers.fire,
     entries: [],
   },
   {
-    id: 13,
+    id: 14,
     name: "Trailhead to Preschool",
     isLandmark: true,
     position: {
       lat: 39.69782547715623,
       lng: -84.30554250855133,
     },
-    image: "",
+    image: null,
     marker: markers.trail,
     entries: [],
   },
   {
-    id: 14,
+    id: 15,
     name: "Nature Preschool",
     isLandmark: true,
     position: {
       lat: 39.69833030551913,
       lng: -84.30776681747716,
     },
-    image: "",
+    image: null,
     marker: markers.school,
     entries: [],
   },
   {
-    id: 15,
+    id: 16,
     name: "Preschool Entrance",
     isLandmark: true,
     position: {
       lat: 39.69814623917417,
       lng: -84.30895976599574,
     },
-    image: "",
+    image: null,
     marker: markers.road,
     entries: [],
   },
   {
-    id: 16,
+    id: 17,
     name: "Trailhead to Tire Swing & Observation Deck",
     isLandmark: true,
     position: {
       lat: 39.69766082604324,
       lng: -84.30381321074795,
     },
-    image: "",
+    image: null,
     marker: markers.trail,
     entries: [],
   },
@@ -295,9 +295,162 @@ var locations = [
 var entries = [
   {
     id: 0,
-    title: "",
-    location: 0,
-    text: "",
+    title: 'Welcome to the Learning Tree Farm!',
+    location: locations[0], // Main Entrance
+    text: 'The large sign on the side of the road says, "Welcome to the Learning Tree Farm."',
+    actions: ['Turn into the driveway.'],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 1,
+    title: 'Small field for event parking.',
+    location: locations[1], // Event Parking Lot
+    text: 'A small field on the side of the driveway that is intended for large event parking.',
+    actions: ['Continue up the driveway.'],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 2,
+    title: 'Primary parking for our visitors.',
+    location: locations[2], // Parking Lot
+    text: 'The primary parking area is located next to the driveway before the loop.',
+    actions: ['Find a parking spot.', 'Drive around the loop.'],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 3,
+    title: '',
+    location: locations[3], // Information
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 4,
+    title: '',
+    location: locations[4], // Century House
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 5,
+    title: '',
+    location: locations[5], // Restrooms
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 6,
+    title: '',
+    location: locations[6], // Smokehouse
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 7,
+    title: '',
+    location: locations[7], // Bank Barn
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 8,
+    title: '',
+    location: locations[8], // Tobacco Barn
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 9,
+    title: '',
+    location: locations[9], // Chicken Coop
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 10,
+    title: '',
+    location: locations[10], // Garden
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 11,
+    title: '',
+    location: locations[11], // Play House & Tractor
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 12,
+    title: '',
+    location: locations[12], // Pavilion
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 13,
+    title: '',
+    location: locations[13], // Bonfire Pit
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 14,
+    title: '',
+    location: locations[14], // Trailhead to Preschool
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 15,
+    title: '',
+    location: locations[15], // Nature Preschool
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 16,
+    title: '',
+    location: locations[16], // Preschool Entrance
+    text: '',
+    actions: [],
+    requirements: [],
+    expiration: []
+  },
+  {
+    id: 17,
+    title: '',
+    location: locations[17], // Trailhead to Tire Swing & Observation Deck
+    text: '',
     actions: [],
     requirements: [],
     expiration: []
@@ -309,7 +462,7 @@ var items = [
     id: 0,
     name: "",
     location: 0,
-    image: "",
+    image: null,
     entries: []
   }
 ]
