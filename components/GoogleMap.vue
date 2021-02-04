149a909ab9bf1b644442b7347e01e51e463fa584
Christopher Ritter <chris@christopherritter.com>
2021-02-04 18:09:16 -0500

    added map style

--- components/GoogleMap.vue
+++ components/GoogleMap.vue
@@ -5,7 +5,7 @@
     language="en"
     :cluster="{ options: { styles: clusterStyle } }"
     :center="{ lat: position.lat, lng: position.lng }"
-    :options="{ fullscreenControl: false }"
+    :options="{fullscreenControl: false, styles: mapStyle}"
     :zoom="zoom"
     class="fill-height"
     height="100%"
@@ -18,7 +18,9 @@
       @click="currentLocation = location"
     >
       <GMapInfoWindow :options="{ maxWidth: 200 }">
-        <code> lat: {{ location.position.lat }}, lng: {{ location.position.lng }} </code>
+        <code>
+          lat: {{ location.position.lat }}, lng: {{ location.position.lng }}
+        </code>
       </GMapInfoWindow>
     </GMapMarker>
     <!-- <GMapCircle :options="circleOptions" /> -->
@@ -35,25 +37,141 @@ export default {
       // circleOptions: {
       //   ...
       // },
-      // locations: [
-      //   {
-      //     lat: 44.933076,
-      //     lng: 15.629058
-      //   },
-      //   {
-      //     lat: 45.815,
-      //     lng: "15.9819"
-      //   },
-      //   {
-      //     lat: "45.12",
-      //     lng: "16.21"
-      //   }
-      // ],
       // pins: {
       //   selected: "data:image/png;base64,iVBORw0KGgo...",
       //   notSelected: "data:image/png;base64,iVBORw0KGgo..."
       // },
-      // mapStyle: [...],
+      mapStyle: [
+        {
+          featureType: "water",
+          stylers: [
+            {
+              saturation: 43
+            },
+            {
+              lightness: -11
+            },
+            {
+              hue: "#0088ff"
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              hue: "#ff0000"
+            },
+            {
+              saturation: -100
+            },
+            {
+              lightness: 99
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.stroke",
+          stylers: [
+            {
+              color: "#808080"
+            },
+            {
+              lightness: 54
+            }
+          ]
+        },
+        {
+          featureType: "landscape.man_made",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#ece2d9"
+            }
+          ]
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#ccdca1"
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#767676"
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              color: "#ffffff"
+            }
+          ]
+        },
+        {
+          featureType: "poi",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "landscape.natural",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              visibility: "on"
+            },
+            {
+              color: "#b8cb93"
+            }
+          ]
+        },
+        {
+          featureType: "poi.park",
+          stylers: [
+            {
+              visibility: "on"
+            }
+          ]
+        },
+        {
+          featureType: "poi.sports_complex",
+          stylers: [
+            {
+              visibility: "on"
+            }
+          ]
+        },
+        {
+          featureType: "poi.medical",
+          stylers: [
+            {
+              visibility: "on"
+            }
+          ]
+        },
+        {
+          featureType: "poi.business",
+          stylers: [
+            {
+              visibility: "simplified"
+            }
+          ]
+        }
+      ],
       clusterStyle: [
         {
           url:
@@ -67,8 +185,8 @@ export default {
   },
   props: ["questId", "position", "location", "zoom"],
   computed: {
-    locations () {
-      return this.$store.state.locations
+    locations() {
+      return this.$store.state.locations;
     }
   },
   // mounted() {
