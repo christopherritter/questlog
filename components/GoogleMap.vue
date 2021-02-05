208fd5d152bb6b712006101168eb2cf4182e4f98
Christopher Ritter <chris@christopherritter.com>
2021-02-04 20:54:33 -0500

    modified GMapInfoWindow style

--- components/GoogleMap.vue
+++ components/GoogleMap.vue
@@ -14,10 +14,10 @@
       :key="location.id"
       :position="{ lat: location.position.lat, lng: location.position.lng }"
       :options="{ icon: { url: require('~/assets/img/' + location.marker), anchor: { x: 25, y: 25 } } }"
-      @click="currentLocation = location"
+      @click="$emit('view-location', location.id)"
     >
       <GMapInfoWindow :options="{ maxWidth: 200 }">
-        <code>
+        <code class="grey--text text--darken-4">
           lat: {{ location.position.lat }}, lng: {{ location.position.lng }}
         </code>
       </GMapInfoWindow>
@@ -179,80 +179,6 @@ export default {
       return this.$store.state.locations;
     }
   },
-  // mounted() {
-  //   // if (!process.server) {
-  //   if (typeof google === "undefined") {
-  //     const script = document.createElement("script");
-  //     script.onload = this.onScriptLoaded;
-  //     script.type = "text/javascript";
-  //     script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
-  //     document.head.appendChild(script);
-  //   } else {
-  //     this.onScriptLoaded();
-  //   }
-
-  //   // }
-  // },
-  // methods: {
-  //   onScriptLoaded(event = null) {
-  //     const quest = this.$store.state.quests[this.questId];
-  //     const region = this.$store.state.regions[quest.region];
-
-  //     // The map, centered at the Learning Tree Farm
-  //     const map = new google.maps.Map(document.getElementById("map"), {
-  //       zoom: region.zoom,
-  //       center: region.position,
-  //       mapId: "b5c77f93da5a90ff"
-  //     });
-  //     // The marker, positioned at the Learning Tree Farm
-  //     // const marker = new google.maps.Marker({
-  //     //   position: this.$store.state.quests[this.questId].region.position,
-  //     //   map: map
-  //     // });
-
-  //     const locations = this.$store.state.locations;
-  //     var infowindow = new google.maps.InfoWindow({});
-  //     var marker, count;
-
-  //     for (count = 0; count < locations.length; count++) {
-  //       marker = new google.maps.Marker({
-  //         position: new google.maps.LatLng(
-  //           locations[count].position.lat,
-  //           locations[count].position.lng
-  //         ),
-  //         icon: {
-  //           url: require("~/assets/img/" + locations[count].marker),
-  //           anchor: new google.maps.Point(25, 25)
-  //         },
-  //         map: map,
-  //         title: locations[count].name
-  //       });
-  //       google.maps.event.addListener(
-  //         marker,
-  //         "click",
-  //         (function(marker, count) {
-  //           return function() {
-  //             infowindow.setContent(
-  //               '<h4 class="grey--text text--darken-4">' +
-  //                 locations[count].name +
-  //                 "</h4>"
-  //             );
-  //             infowindow.open(map, marker);
-  //           };
-  //         })(marker, count)
-  //       );
-  //     }
-  //   }
-  // },
-  watch: {
-    position(newVal, oldVal) {
-      // var map = google.maps.Map(document.getElementById("map"));
-      // var latLng = new google.maps.LatLng(newVal.lat, newVal.lng);
-
-      console.log(newVal.lat, newVal.lng);
-      // map.setCenter(latLng);
-    }
-  }
 };
 </script>
 
