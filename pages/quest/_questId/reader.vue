6d26c3f5a86b1fe57c7947a97e2ce3ec938f2450
Christopher Ritter <chris@christopherritter.com>
2021-02-07 17:47:20 -0500

    removed max height css

--- pages/quest/_questId/reader.vue
+++ pages/quest/_questId/reader.vue
@@ -107,15 +107,3 @@ export default {
   }
 };
 </script>
-
-<style>
-#QuestSidebar {
-  max-height: calc(100vh - 100px);
-  overflow-y: auto;
-}
-@media only screen and (max-width: 960px) {
-  #QuestSidebar {
-    max-height: 65vh;
-  }
-}
-</style>
