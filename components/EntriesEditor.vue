ef0ddaf4c1e13bb49c6e2c494b1b0f85ac05cbc6
Christopher Ritter <chris@christopherritter.com>
2021-02-14 14:03:52 -0500

    additional layout for editor components

--- components/EntriesEditor.vue
+++ components/EntriesEditor.vue
@@ -2,10 +2,19 @@
   <v-card flat>
     <v-card-text>
       <v-row>
-        <v-col cols="4">
+        <v-col cols="12" md="5" lg="4">
           <v-row>
             <v-col>
-              <h1 class="my-6">Entries</h1>
+              <div class="d-flex mt-5 mb-4 align-center">
+                <h1>Entries</h1>
+                <v-spacer></v-spacer>
+                <v-btn text>
+                  <v-icon class="mr-2" dark>
+                    mdi-plus-circle
+                  </v-icon>
+                  New
+                </v-btn>
+              </div>
               <v-text-field
                 v-model="entry.title"
                 label="Title"
@@ -18,13 +27,75 @@
                 outlined
               ></v-textarea>
               <v-select :items="actions" label="Actions" outlined></v-select>
-              <v-select :items="objectives" label="Requirements" outlined></v-select>
-              <v-select :items="objectives" label="Expiration" outlined></v-select>
+              <v-select
+                :items="objectives"
+                label="Requirements"
+                outlined
+              ></v-select>
+              <v-select
+                :items="objectives"
+                label="Expiration"
+                outlined
+              ></v-select>
+              <div class="d-flex justify-end">
+                <v-btn dark outlined disabled>Reset</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn v-if="!isSelected" dark outlined @click="addEntry()"
+                  >Add</v-btn
+                >
+                <v-btn v-else dark outlined color="primary">Update</v-btn>
+              </div>
             </v-col>
           </v-row>
         </v-col>
-        <v-col>
-          <h1 class="my-6">List</h1>
+        <v-col cols="12" md="7" lg="8" class="d-flex flex-column">
+          <div class="d-flex flex-shrink-0 mt-5 mb-4 align-center">
+            <v-btn text dark :disabled="!isSelected">
+              <v-icon class="mr-2">
+                mdi-pencil
+              </v-icon>
+              Edit
+            </v-btn>
+            <v-btn text dark :disabled="!isSelected">
+              <v-icon class="mr-2">
+                mdi-delete
+              </v-icon>
+              Remove
+            </v-btn>
+          </div>
+          <div v-if="entries.length <= 0">
+            <v-card outlined>
+              <v-card-text>
+                Add entries to your quest with the form on the left.
+              </v-card-text>
+            </v-card>
+          </div>
+          <v-radio-group class="flex-shrink-0" v-model="radioGroup">
+            <v-radio
+              v-for="(entry, index) in entries"
+              :key="index"
+              :label="entry.title"
+              :value="index"
+              @click="selectEntry(index)"
+            ></v-radio>
+          </v-radio-group>
+          <div class="d-flex flex-grow-1 flex-shrink-1 align-end justify-end">
+            <v-btn outlined dark @click="$emit('change-tab', 'locations')">
+              Back
+            </v-btn>
+            <v-btn
+              outlined
+              dark
+              class="ml-2"
+              @click="$emit('change-tab', 'items')"
+            >
+              Next
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn dark disabled color="primary" class="ml-2">
+              Save
+            </v-btn>
+          </div>
         </v-col>
       </v-row>
     </v-card-text>
@@ -40,10 +111,27 @@ export default {
         title: "",
         text: ""
       },
-      entries: []
+      entries: [],
+      radioGroup: null
     };
   },
-  props: [ 'locations', 'actions', 'objectives' ]
+  props: ["locations", "actions", "objectives"],
+  computed: {
+    isSelected() {
+      if (this.entry == this.entries[this.radioGroup]) return true;
+      return false;
+    }
+  },
+  methods: {
+    addEntry() {
+      let newEntry = JSON.parse(JSON.stringify(this.entry));
+      this.entries.push(newEntry);
+      this.radioGroup = this.entries.length;
+    },
+    selectEntry(index) {
+      this.entry = this.entries[index];
+    }
+  }
 };
 </script>
 
--- components/ItemsEditor.vue
+++ components/ItemsEditor.vue
@@ -2,10 +2,19 @@
   <v-card flat>
     <v-card-text>
       <v-row>
-        <v-col cols="4">
+        <v-col cols="12" md="5" lg="4">
           <v-row>
             <v-col>
-              <h1 class="my-6">Items</h1>
+              <div class="d-flex mt-5 mb-4 align-center">
+                <h1>Items</h1>
+                <v-spacer></v-spacer>
+                <v-btn text>
+                  <v-icon class="mr-2" dark>
+                    mdi-plus-circle
+                  </v-icon>
+                  New
+                </v-btn>
+              </div>
               <v-text-field
                 v-model="item.name"
                 label="Name"
@@ -18,11 +27,65 @@
                 outlined
               ></v-text-field>
               <v-select :items="entries" label="Entries" outlined></v-select>
+              <div class="d-flex justify-end">
+                <v-btn dark outlined disabled>Reset</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn v-if="!isSelected" dark outlined @click="addEntry()"
+                  >Add</v-btn
+                >
+                <v-btn v-else dark outlined color="primary">Update</v-btn>
+              </div>
             </v-col>
           </v-row>
         </v-col>
-        <v-col>
-          <h1 class="my-6">List</h1>
+        <v-col cols="12" md="7" lg="8" class="d-flex flex-column">
+          <div class="d-flex flex-shrink-0 mt-5 mb-4 align-center">
+            <v-btn text dark :disabled="!isSelected">
+              <v-icon class="mr-2">
+                mdi-pencil
+              </v-icon>
+              Edit
+            </v-btn>
+            <v-btn text dark :disabled="!isSelected">
+              <v-icon class="mr-2">
+                mdi-delete
+              </v-icon>
+              Remove
+            </v-btn>
+          </div>
+          <div v-if="entries.length <= 0">
+            <v-card outlined>
+              <v-card-text>
+                Add entries to your quest with the form on the left.
+              </v-card-text>
+            </v-card>
+          </div>
+          <v-radio-group class="flex-shrink-0" v-model="radioGroup">
+            <v-radio
+              v-for="(entry, index) in entries"
+              :key="index"
+              :label="entry.title"
+              :value="index"
+              @click="selectEntry(index)"
+            ></v-radio>
+          </v-radio-group>
+          <div class="d-flex flex-grow-1 flex-shrink-1 align-end justify-end">
+            <v-btn outlined dark @click="$emit('change-tab', 'locations')">
+              Back
+            </v-btn>
+            <v-btn
+              outlined
+              dark
+              class="ml-2"
+              @click="$emit('change-tab', 'items')"
+            >
+              Next
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn dark disabled color="primary" class="ml-2">
+              Save
+            </v-btn>
+          </div>
         </v-col>
       </v-row>
     </v-card-text>
@@ -38,10 +101,27 @@ export default {
         name: "",
         image: "",
       },
-      items: []
+      items: [],
+      radioGroup: null
     };
   },
-  props: ['locations', 'entries']
+  props: ['locations', 'entries'],
+  computed: {
+    isSelected() {
+      if (this.item == this.items[this.radioGroup]) return true;
+      return false;
+    }
+  },
+  methods: {
+    addItem() {
+      let newItem = JSON.parse(JSON.stringify(this.item));
+      this.items.push(newItem);
+      this.radioGroup = this.items.length;
+    },
+    selectItem(index) {
+      this.item = this.items[index];
+    }
+  }
 };
 </script>
 
